4acb3c8485d4076dbc087d3b3a04dd928ce01066
Trung Nguyen <thanhtrung.nguyen@zuhlke.com>
2026-05-08 10:25:01 +0700

    init commit

---
+++ src/main/kotlin/com/ttng/demo_web_auth/DemoWebAuthApplication.kt
@@ -0,0 +1,11 @@
+package com.ttng.demo_web_auth
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemoWebAuthApplication
+
+fun main(args: Array<String>) {
+	runApplication<DemoWebAuthApplication>(*args)
+}
---
+++ src/test/kotlin/com/ttng/demo_web_auth/DemoWebAuthApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ttng.demo_web_auth
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoWebAuthApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
